fff974bbc29b558610a37b0664b8b96b9a7ada0f
Trish Griego <trish.griego@moz.com>
2017-03-27 16:58:30 -0700

    Attempted to add Material UI to project. Currently getting an error regarding injectTapEventPlugin.js.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import 'react-virtualized/styles.css'
 // Using an ES6 transpiler like Babel
 import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 // const var blue = "#070F4E"
 // const lighterBlue = "#2772DB"
