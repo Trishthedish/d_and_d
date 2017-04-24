924ab53b705c02a627deb32477a263fc905b36a4
Trish Griego <trish.griego@moz.com>
2017-04-24 16:53:17 -0700

    Began adding similiar redux logic found in example app. Nothing is working properly. Hard to trasnlate into redux with sortable ability that was created in earlier iterations.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,23 @@
 import React from 'react'
+import FilterLink from '../containers/FilterLink'
+// TODO: make this footer more relevant. Instead of show all show active.
+// These could be show google etc.
+const Footer = () => (
+  <p>
+    Show:
+    {" "}
+    <FilterLink filter="SHOW_ALL">
+      All
+    </FilterLink>
+    {", "}
+    <FilterLink filter="SHOW_ACTIVE">
+      Active
+    </FilterLink>
+    {", "}
+    <FilterLink filter="SHOW_COMPLETED">
+      Completed
+    </FilterLink>
+  </p>
+)
+
+export default Footer
--- src/components/Rule.js
+++ src/components/Rule.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 
 const Rule = ({ onClick, text}) => (
-  
+
 )
