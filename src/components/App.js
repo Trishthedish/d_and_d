d8104b7b82f06ae0096c88a7c246900c6ae28817
Trish Griego <trish.griego@moz.com>
2017-04-24 14:10:00 -0700

    still strucuturing out my redux file

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+import AddRule from '../containers/AddRule'
+import VisibleRuleList from '../containers/VisibleRuleList'
+
+const App = () => (
+  <div>
+    <AddRule />
+    <VisibleRuleList />
+    <footer />
+  </div>
+)
+
+export default App
