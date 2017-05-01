1ae3f24b786e69e8146d438a39ca17cb11fcd6cc
Trish Griego <trish.griego@moz.com>
2017-05-01 14:08:03 -0700

    Added update_modal action. Table is still rendering correctly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 export const UPDATE_NAME = 'UPDATE_NAME'
 export const UPDATE_ENGINE = 'UPDATE_ENGINE'
-
+export const UPDATE_MODAL = 'UPDATE_MODAL'
 export function updateName (name) {
   return {
     type: UPDATE_NAME,
@@ -12,5 +12,12 @@ export function updateEngine(engine) {
   return {
     type: UPDATE_ENGINE,
     engine
+  }
+}
+
+export function updateModal(editModalVisibility) {
+  return {
+    type: UPDATE_MODAL,
+    editModalVisibility
   }
 }
