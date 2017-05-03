645f92d54ce28d2e13dd7ed2aaaf5ade4dd97829
Trish Griego <trish.griego@moz.com>
2017-05-03 15:46:12 -0700

    Changed all input tags within the rule edit modal to be textarea tags instead of input tags.

--- src/components/RuleEditModal.js
+++ src/components/RuleEditModal.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Modal from 'react-modal'
 import RaisedButton from 'material-ui/RaisedButton';
-import {updateEditingRule} from '../actions'
+// import {updateEditingRule} from '../actions'
 
 class RuleEditModal extends Component {
 
@@ -25,38 +25,38 @@ console.log()
           onRequestClose={()=>{console.log("requestedClose")}}
           contentLabel="Edit Rule Modal"
           shouldCloseOnOverlayClick={true}>
-          <h1>Modal Content</h1>
+          <h1>Editing Swappable Rule</h1>
           <form onSubmit={onFormSave}>
              <p>Engine</p>
-              <input type='text' value={rule.engine}
+              <textarea type='text' value={rule.engine}
                 onChange={(event) => onRuleEdit('engine', event.target.value)}
                 />
              <p>Local Regex</p>
-              <input type='text' value={rule.locale_regex}
+              <textarea type='text' value={rule.locale_regex}
                 onChange={(event) => onRuleEdit('locale_regex', event.target.value)}
                 />
              <p>Strategy</p>
-              <input type="text" value={rule.strategy}
+              <textarea type="text" value={rule.strategy}
                 onChange={(event) => onRuleEdit('strategy', event.target.value)}
                 />
              <p>Keyword Regex</p>
-              <input type="text" value={rule.keyword_regex}
+              <textarea type="text" value={rule.keyword_regex}
                 onChange={(event) => onRuleEdit('keyword_regex', event.target.value)}
                 />
              <p>Max Tries</p>
-              <input type="text" value={rule.max_tries}
+              <textarea type="text" value={rule.max_tries}
                 onChange={(event) => onRuleEdit('max_tries', event.target.value)}/>
              <p>Proxy Balancer</p>
-                <input type="text" value={rule.proxy_bal}
+                <textarea type="text" value={rule.proxy_bal}
                   onChange={(event) => onRuleEdit('proxy_bal', event.target.value)}/>
              <p>Destination Scraper</p>
-                <input type="text" value={rule.dest_scraper}
+                <textarea type="text" value={rule.dest_scraper}
                   onChange={(event) => onRuleEdit('dest_scraper', event.target.value)}/>
              <p>Overwrite</p>
-                <input type="text" value={rule.overwrite}
+                <textarea type="text" value={rule.overwrite}
                   onChange={(event) => onRuleEdit('overwrite', event.target.value)}/>
              <p>Raw HTML Source</p>
-                <input type="text" value={rule.raw_html}
+                <textarea type="text" value={rule.raw_html}
                   onChange={(event) => onRuleEdit('raw_html', event.target.value)}/>
             <div>
               <RaisedButton
