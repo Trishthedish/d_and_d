72148519717fdc4aedc6718e83b33d076c2397f6
Trish Griego <trish.griego@moz.com>
2017-05-02 18:19:14 -0700

    Modal content that was being rendered is present but textarea and select aren't completely working as anticipated.

--- notes.md
+++ notes.md
@@ -82,3 +82,108 @@
     borderRadius               : '4px',
     outline                    : 'none',
     padding                    : '50px'
+
+
+
+
+
+
+
+    import React, {Component} from 'react';
+    import Modal from 'react-modal'
+
+    class RuleEditModal extends Component {
+      render() {
+        return (
+         <div>
+           <Modal
+             isOpen={this.props.editModalVisibility}
+             closeTimeoutMS={10}
+             className="Modal"
+             overlayClassName="Overlay"
+             onAfterOpen={()=>{console.log("opened so easy!")}}
+             onRequestClose={()=>{console.log("requestedClose")}}
+             contentLabel="Edit Rule Modal"
+             ariaHideApp={false}
+             shouldCloseOnOverlayClick={false}>
+
+             <h1>Modal Content</h1>
+            <form>
+              <button>Close Modal</button>
+               <fieldset>
+                 <legend>Choose Preffered Search Engine</legend>
+                 <p>
+                   <input type="radio" name="engines" id="google" value="google"/>
+                   <label for="google">google</label>
+                 </p>
+                 <p>
+                   <input type="radio" name="engines" id="yahoo" value="yahoo"/>
+                   <label for="yahoo">yahoo</label>
+                 </p>
+                 <p>
+                   <input type="radio" name="engines" id="bing" value="bing"/>
+                   <label for="bing">bing</label>
+                 </p>
+                </fieldset>
+
+                <fieldset>
+                  <legend>Local Regex</legend>
+                  <p>
+                    <input value={this.props.engine} />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Strategy</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+
+                <fieldset>
+                  <legend>Keyword Regex</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Subscription</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Max Tries</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Proxy Balancer</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Destination Scraper</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Overwrite</legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+                <fieldset>
+                  <legend>Raw HTML </legend>
+                  <p>
+                    <input />
+                  </p>
+                </fieldset>
+             </form>
+           </Modal>
+         </div>
+        )}
+    }
+    export default RuleEditModal
--- src/components/RuleEditModal.js
+++ src/components/RuleEditModal.js
@@ -1,98 +1,51 @@
 import React, {Component} from 'react';
 import Modal from 'react-modal'
+// import FlatButton from 'material-ui/FlatButton';
+// import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 class RuleEditModal extends Component {
   render() {
+  const labelStyle = {
+    color: 'rgb(41, 128, 185)',
+    // size: 40, no idea how to get size to change.
+  }
+
     return (
-     <div>
-       <Modal
-         isOpen={this.props.editModalVisibility}
-         closeTimeoutMS={10}
-         className="Modal"
-         overlayClassName="Overlay"
-         onAfterOpen={()=>{console.log("opened so easy!")}}
-         onRequestClose={()=>{console.log("requestedClose")}}
-         contentLabel="Edit Rule Modal"
-         ariaHideApp={false}
-         shouldCloseOnOverlayClick={false}>
+      <div>
+        <Modal
+          isOpen={this.props.editModalVisibility}
+          closeTimeoutMS={10}
+          className="Modal"
+          overlayClassName="Overlay"
+          onAfterOpen={()=>{console.log("opened so easy!")}}
+          onRequestClose={()=>{console.log("requestedClose")}}
+          contentLabel="Edit Rule Modal"
+          ariaHideApp={false}
+          shouldCloseOnOverlayClick={false}>
+          <h1>Modal Content</h1>
+         <form>
+           <SelectField
+             floatingLabelText="This is the Engine list"
+             floatingLabelText="Engine"
+             floatingLabelFixed={true}
+             hintText="can we have this prefilled out?"
+             floatingLabelStyle={labelStyle}>
+            <MenuItem value={1} primaryText="Google" />
+            <MenuItem value={2} primaryText="Yaboo" />
+            <MenuItem value={3} primaryText="Bing" />
+           </SelectField>
+           <div style={labelStyle}>
+             locale Regex
+           </div>
 
-         <h1>Modal Content</h1>
-        <form>
-          <button>Close Modal</button>
-           <fieldset>
-             <legend>Choose Preffered Search Engine</legend>
-             <p>
-               <input type="radio" name="engines" id="google" value="google"/>
-               <label for="google">google</label>
-             </p>
-             <p>
-               <input type="radio" name="engines" id="yahoo" value="yahoo"/>
-               <label for="yahoo">yahoo</label>
-             </p>
-             <p>
-               <input type="radio" name="engines" id="bing" value="bing"/>
-               <label for="bing">bing</label>
-             </p>
-            </fieldset>
 
-            <fieldset>
-              <legend>Local Regex</legend>
-              <p>
-                <input value={this.props.engine} />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Strategy</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
 
-            <fieldset>
-              <legend>Keyword Regex</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Subscription</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Max Tries</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Proxy Balancer</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Destination Scraper</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Overwrite</legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
-            <fieldset>
-              <legend>Raw HTML </legend>
-              <p>
-                <input />
-              </p>
-            </fieldset>
          </form>
-       </Modal>
-     </div>
-    )}
-}
-export default RuleEditModal
+        </Modal>
+      </div>
+     )}
+     }
+     export default RuleEditModal
