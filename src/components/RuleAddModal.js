3add1d5873024a223263b30185d13befe0b17f9b
Trish Griego <trish.griego@moz.com>
2017-05-08 18:33:26 -0700

    Added basic form info to addRuleModal.

--- src/components/RuleAddModal.js
+++ src/components/RuleAddModal.js
@@ -16,6 +16,38 @@ class RuleAddModal extends Component {
           onRequestClose={()=>{console.log("Add rule closed")}}
           contentLabel="Add Rule Modal"
           shouldCloseOnOverlayClick={true}>
+          <h1> ADD RULE MODEL</h1>
+          <form>
+            <p>Engine</p>
+              <textarea/>
+            <p>Strategy</p>
+              <textarea/>
+            <p>Keyword Regex</p>
+              <textarea/>
+            <p>Max Tries</p>
+              <textarea/>
+            <p>Proxy Balancer</p>
+              <textarea/>
+            <p>Destination Scraper</p>
+              <textarea/>
+            <p>Overwrite</p>
+              <textarea/>
+            <p>Raw HTML Source</p>
+              <textarea/>
+              <div>
+                <RaisedButton
+                  label="Save Here"
+                  secondary={true}
+
+                  value="Save"/>
+
+              <RaisedButton
+                  label="Cancel Close"
+                  primary={true}
+
+                  value="Close"/>
+              </div>
+          </form>
         </Modal>
       </div>
     )
