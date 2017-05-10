026be8f3596d4c51a02c19f5a21d923bae2f97f2
Trish Griego <trish.griego@moz.com>
2017-05-09 18:43:06 -0700

    removed erroneous jpegs, cleaned up unused comments from App.js. More to do

--- src/App.js
+++ src/App.js
@@ -2,20 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'react-virtualized/styles.css'
-// Using an ES6 transpiler like Babel
-// import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
-// import RaisedButton from 'material-ui/RaisedButton';
-
-// import ActionAndroid from 'material-ui/svg-icons/action/android';
-// import FontIcon from 'material-ui/FontIcon';
-// import {red500, yellow500, blue500, greenA200} from 'material-ui/styles/colors';
-// import google_icon from '../src/google_icon.png';
-// import yahoo_icon from '../src/yahoo_icon.png';
-// import bing_icon from '../src/bing_icon.png';
 import Main from './containers/Main'
-// import {emojify} from 'react-emojione';
 import {createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
@@ -36,45 +24,24 @@ function configureStore(preloadedState) {
 }
 const store = configureStore()
 
-// const engineIcon = {
-//   width: '41px',
-//   height: '40px',
-//   padding: '4px',
-// }
-
-// const iconStyles = {
-//   fontSize: '48px',
-//   height: 40,
-// };
-
-// const buttonStyle = {
-//   margin: 12,
-// }
-
 class App extends Component {
 
   render() {
       console.log("App.js store: ", store)
     return (
       <MuiThemeProvider>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Swappable Remastered</h2>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Swappable Remastered</h2>
+          </div>
+          <Provider store={store}>
+            <Main />
+          </Provider>
         </div>
-        <div>
-      </div>
-        <Provider store={store}>
-          <Main />
-        </Provider>
-
-
-      </div>
-
       </MuiThemeProvider>
     );
   }
 }
-
 
 export default App;
